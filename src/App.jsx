c970e4d7a97577313cc6da67abf53fff333f7bd4
Vagishmaurya <89206689+Vagishmaurya@users.noreply.github.com>
2023-07-14 13:34:32 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes , Route} from "react-router-dom";
+import Navbar from "../src/components/Navbar"
+import Home from "../src/pages/Home"
+import Cart from "../src/pages/Cart"
+// import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  return <>
+       <div className="mx-0 p-0 relative"> 
+        <div className="bg-slate-900">
+          <Navbar />
+        </div>
+        <Routes >
+           <Route path="/" element ={<Home/>}/>
+            <Route path="/cart" element={<Cart/>}/>
+        </Routes>
+        </div>
+  </>;
+};
+
+export default App;
